36482503c68f566d1ef7682ee0347dd26633ccb5
Sokritha Yen <sokritha17@gmail.com>
2020-11-07 17:59:02 +0700

    updating our app with a shop page, collection preview component and collection item component

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+        className="background-image"
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(MenuItem);
